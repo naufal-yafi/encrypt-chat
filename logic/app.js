324c3a166659b2647358379afc9cee5d5a9925a2
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2022-05-08 16:25:33 +0700

    bug fixed undifined

--- logic/app.js
+++ logic/app.js
@@ -9,7 +9,7 @@ consolAlert = 'input is not in accordance with the provisions',
 data = [
     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
     'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-    '1','2','3','4','5','6','7','8','9','0'
+    '1','2','3','4','5','6','7','8','9','0',' '
 ];
 
 let strToChar = [],
@@ -50,8 +50,11 @@ const changeKey = (pass) => {
 // 4. convert key number
 const conv = () => {
     for (let i=0; i<keyChat.length; i++){
-        let pK = i+1,
-        indic = keyChat[pK-1];
+        indic = keyChat[i];
+        if (indic > (data.length-1)){
+            let cond = indic-(data.length-1);
+            indic = cond;
+        }
         strToChar[i] = data[indic];
     }
 };
@@ -135,6 +138,7 @@ const success = () => {
 btnEnc.addEventListener('click',() => {
     let valueChat = inputChat.value,
     valuePass = inputPswd.value;
+    clean();
     
     if (valueChat == '' && valuePass == ''){
         // if the user does not fill in the password and chat
